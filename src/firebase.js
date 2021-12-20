faad6947f10685450fb7ed2ba6acf564103ff98b
easxin <codingericwang@gmail.com>
2021-12-20 01:42:36 -0500

    init commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCG5UlgBecUdrADuhg7z1mK0qwkkIio_zE",
+    authDomain: "whatsapp-a227a.firebaseapp.com",
+    projectId: "whatsapp-a227a",
+    storageBucket: "whatsapp-a227a.appspot.com",
+    messagingSenderId: "678190812838",
+    appId: "1:678190812838:web:a81fe788d0de471b0eae67"
+  };
